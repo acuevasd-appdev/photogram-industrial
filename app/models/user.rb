f4ce744db58d7e48f6c5d6be9f8b7fa83cb43756
acuevasd <acuevasd@chicagobooth.edu>
2023-01-14 23:11:37 +0000

    counts added

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,24 @@
 # Table name: users
 #
 #  id                     :bigint           not null, primary key
+#  comments_count         :integer          default(0)
 #  email                  :citext           default(""), not null
 #  encrypted_password     :string           default(""), not null
-#  reset_password_token   :string
-#  reset_password_sent_at :datetime
-#  remember_created_at    :datetime
-#  username               :citext
-#  private                :boolean
 #  likes_count            :integer          default(0)
-#  comments_count         :integer          default(0)
+#  photos_count           :integer          default(0)
+#  private                :boolean
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  username               :citext
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#  index_users_on_username              (username) UNIQUE
 #
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
@@ -21,9 +28,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :own_photos, class_name: "Photo", foreign_key: "owner_id", counter_cache: true
-  has_many :comments, foreign_key: "author_id", counter_cache: true
-  has_many :followers, foreign_key: "recipient_id", counter_cache: true
-  has_many :following, foreign_key: "sender_id", counter_cache: true
+  has_many :own_photos, class_name: "Photo", foreign_key: "owner_id"
+  has_many :comments, foreign_key: "author_id"
+  has_many :followers, foreign_key: "recipient_id"
+  has_many :following, foreign_key: "sender_id"
   has_many :likes, foreign_key: "fan_id"
 end
