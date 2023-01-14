b69cb185a162237b67a465e29cafb001c7692788
acuevasd <acuevasd@chicagobooth.edu>
2023-01-14 22:49:29 +0000

    addingcounts

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,6 @@
 class Photo < ApplicationRecord
   belongs_to :owner, class_name: "User"
 
-  has_many :comments
-  has_many :likes
+  has_many :comments, counter_cache: true
+  has_many :likes, counter_cache: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :own_photos, class_name: "Photo", foreign_key: "owner_id"
-  has_many :comments, foreign_key: "author_id"
-  has_many :followers, foreign_key: "recipient_id"
-  has_many :following, foreign_key: "sender_id"
+  has_many :own_photos, class_name: "Photo", foreign_key: "owner_id", counter_cache: true
+  has_many :comments, foreign_key: "author_id", counter_cache: true
+  has_many :followers, foreign_key: "recipient_id", counter_cache: true
+  has_many :following, foreign_key: "sender_id", counter_cache: true
   has_many :likes, foreign_key: "fan_id"
 end
